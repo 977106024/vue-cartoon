f7cec3a1ca0a51024405ce924d1636229c7076b7
bohong <977106024@qq.com>
2020-06-17 21:03:25 +0800

    添加图片处理依赖exif

--- src/components/uploadPhoto.vue
+++ src/components/uploadPhoto.vue
@@ -23,6 +23,7 @@
 
 <script>
 import {upload,getCartoon} from '@/service/getData'
+import Exif from 'exif-js' //图片处理
 export default {
   name: 'uploadPhoto',
   props: {
@@ -34,7 +35,7 @@ export default {
     imgUrl:null,
     success:false,
     loading:false,
-    id:null
+    id:null,
   }),
   created(){
    
@@ -46,20 +47,25 @@ export default {
       this.success = false
 
       const file = e.target.files[0]
-      this.preview(file)
-      const formData = new FormData()
-      formData.append('photo',file)
-
-
-      upload(formData).then(res=>{
+      this.imgPreview(file,(formData)=>{
+         upload(formData).then(res=>{
         if(res.status === 200){
           this.imgUrl = res.data.img
+          this.reviewImg = 'data:image/jpeg;base64,' + this.imgUrl
           this.id = res.data.id
           this.loading = false
           this.success = true
         }
       })
+      })
+
+      // return
+      // this.preview(file)
+      // const formData = new FormData()
+      // formData.append('photo',file)
+
     },
+    //转成base64预览
     preview(file){
 
        var reads = new FileReader()
@@ -86,7 +92,103 @@ export default {
             this.loading = false
         }
       })
+    },
+    //图片处理函数
+    imgPreview (file, callback) {
+      const that = this
+      // 创建一个reader
+      const reader = new FileReader()
+      const img = new Image()
+
+      if (!file || !window.FileReader) return
+      if (/^image/.test(file.type)) {
+        // eslint-disable-next-line
+        let Orientation = null
+        // 去获取拍照时的信息，解决拍出来的照片旋转问题
+        Exif.getData(file, () => {
+          Orientation = Exif.getTag(file, 'Orientation')
+        })
+
+        // 读取成功后的回调
+        reader.onloadend = function () {
+          console.log('==this.result===', this.result)
+          const result = this.result
+          img.src = result
+          img.onload = function () {
+            console.log('===Orientation===', Orientation)
+            console.log('===img===', img)
+            const data = that.compress(img, 0.7, Orientation)
+            const blob = that.dataURItoBlob(data)
+            const formData = new FormData()
+            var nameImg = new Date().getTime() + '.jpg'
+            formData.append('photo', blob, nameImg)
+            // 发送请求
+            // 代码自己写把formData传给后台就可以了
+            callback(formData)
+          }
+        }
+        // 将图片转成base64格式
+        reader.readAsDataURL(file)
+      } else {
+        this.$toast('请上传正确的图片格式')
+      }
+},
+// 压缩图片
+compress (img, compressNum, Orientation) {
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  const width = img.width
+  const height = img.height
+  canvas.width = width
+  canvas.height = height
+  // 铺底色
+  ctx.fillStyle = '#fff'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  if (Orientation && Orientation !== 1) {
+    switch (Orientation) {
+      case 6:
+        canvas.width = height
+        canvas.height = width
+        ctx.rotate(Math.PI / 2)
+        ctx.drawImage(img, 0, -height, width, height)
+        break
+      case 3:
+        ctx.rotate(Math.PI)
+        ctx.drawImage(img, -width, -height, width, height)
+        break
+      case 8:
+        canvas.width = height
+        canvas.height = width
+        ctx.rotate(3 * Math.PI / 2)
+        ctx.drawImage(img, -width, 0, width, height)
+        break
     }
+  } else {
+    ctx.drawImage(img, 0, 0, width, height)
+  }
+
+  // 进行压缩 compressNum - 图片质量:取值范围为0到1
+  const ndata = canvas.toDataURL('image/jpeg', compressNum)
+  return ndata
+},
+// base64转成bolb对象
+dataURItoBlob (base64Data) {
+  var byteString
+  if (base64Data.split(',')[0].indexOf('base64') >= 0) {
+    byteString = atob(base64Data.split(',')[1])
+  } else {
+    byteString = unescape(base64Data.split(',')[1])
+  }
+  var mimeString = base64Data
+    .split(',')[0]
+    .split(':')[1]
+    .split(';')[0]
+  var ia = new Uint8Array(byteString.length)
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i)
+  }
+  return new Blob([ia], { type: mimeString })
+}
   }
 }
 </script>
